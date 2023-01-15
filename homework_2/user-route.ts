a84f89ec145badc5dfe76af7f97caeefc9860317
Nollili <noemilili@gmail.com>
2023-01-15 13:00:40 +0100

    fix password validation

--- homework_2/user-route.ts
+++ homework_2/user-route.ts
@@ -16,7 +16,7 @@ type User = {
 const userSchema = Joi.object().keys({
 	id: Joi.string().required(),
 	login: Joi.string().alphanum().min(3).max(30).required(),
-	password: Joi.string().pattern(new RegExp('^[a-zA-Z0-9]{3,30}$')).required(),
+	password: Joi.string().pattern(new RegExp('(?=.*[A-Za-z])(?=.*\d)[A-Za-z0-9]{3,30}$')).required(),
 	age: Joi.number().integer().min(4).max(130).required(),
 	isDeleted: Joi.boolean().required(),
 });
