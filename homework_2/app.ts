9c7e46ff72bb5636267295e532ea218f05bf6709
Nollili <noemilili@gmail.com>
2023-03-04 18:02:37 +0100

    add cors

--- homework_2/app.ts
+++ homework_2/app.ts
@@ -2,6 +2,7 @@ import * as dotenv from 'dotenv';
 dotenv.config();
 import express from 'express';
 import cookieParser from 'cookie-parser';
+import cors from 'cors';
 import userRouter from './routes/user-routes';
 import groupRouter from './routes/group-routes';
 import { errorHandler } from './middlewares/error-handling';
@@ -12,6 +13,7 @@ const port = 3000;
 
 app.set('strict routing', true);
 app.disable('x-powered-by');
+app.use(cors());
 
 createLogger({
 	transports: [
